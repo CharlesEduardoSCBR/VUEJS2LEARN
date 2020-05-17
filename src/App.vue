dc0f83497d82c55ece32114c04bf18920b17a298
CharlesEduardo <charles.eduardo@webmaissistemas.com.br>
2020-05-17 18:50:17 -0300

    Proposta para desafio 1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <Desafio1/>
   </div>
 </template>
 
 <script>
+import Desafio1 from './components/Desafio1.vue';
+
 
 export default {
   name: 'App',
   components: {
-    
+    Desafio1
   }
 }
 </script>
