7a1b18873c150e1e5faf5c5480f5f09f370d65b2
CharlesEduardo <charles.eduardo@webmaissistemas.com.br>
2020-05-17 22:59:52 -0300

    Deixando mais bonitinho o visual...

--- src/components/Desafio3.vue
+++ src/components/Desafio3.vue
@@ -1,25 +1,58 @@
 <template>
     <div id="desafio3" class="container-fluid">
-        <div class="row-fluid">
-                <!-- 1) Exibir em "resultado" o texto 'Valor Diferente' enquanto
-                "valor" for diferente de 37 - "valor" é alterado pelos botões.
-                Mostrar 'Valor Igual' quando "valor" for igual a 37 -->
+        <!-- 1) Exibir em "resultado" o texto 'Valor Diferente' enquanto
+               "valor" for diferente de 37 - "valor" é alterado pelos botões.
+               Mostrar 'Valor Igual' quando "valor" for igual a 37 -->
 
-            <!-- 2) Monitorar as mudança de "resultado" e reiniciar "valor"
-                para 0 depois de 5 segundos (dica: setTimeout(..., 5000) -->            
-            <div>
-                <p>Valor atual: {{ valor }}</p>
-                <button @click="valor += 5">Somar 5</button>
-                <button @click="valor += 1">Somar 1</button>
-                <p>{{ resultado }}</p>
-            </div>
+        <!-- 2) Monitorar as mudança de "resultado" e reiniciar "valor"
+            para 0 depois de 5 segundos (dica: setTimeout(..., 5000) -->            
+        <div class="row-fluid">
+            <p class="
+                        col-lg-offset-1 col-lg-11
+                        col-md-offset-1 col-md-11
+                        col-sm-offset-1 col-sm-11
+                        col-xs-offset-1 col-xs-11"
+            >Valor atual: {{ valor }}</p>
+            <button @click="valor += 5"
+                class=
+                    "
+                        col-lg-offset-1 col-lg-1
+                        col-md-offset-1 col-md-1
+                        col-sm-offset-1 col-sm-3
+                        col-xs-12
+                        btn btn-primary
+                    "
+            >Somar 5</button>
+            <button @click="valor += 1"
+                class=
+                    "
+                        col-lg-1 col-lg-offset-1
+                        col-md-1 col-md-offset-1
+                        col-sm-3 col-sm-offset-1
+                        col-xs-12
+                        btn btn-primary
+                    "
+            >Somar 1</button>
+            <p class="
+                        col-lg-offset-1 col-lg-11
+                        col-md-offset-1 col-md-11
+                        col-sm-offset-1 col-sm-11
+                        col-xs-offset-1 col-xs-11
+                    "
+            >{{ resultado }}</p>
         </div>
     </div>  
 </template>
 
 <script>
 export default {
-
+    name: 'desafio3',
+    data() {
+        return {
+            valor : 0,
+            resultado : 0,
+        }
+    }
 }
 </script>
 
