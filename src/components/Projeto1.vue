064d827d1d505438c02b6b8f1a97b661f271a10b
CharlesEduardo <charles.eduardo@webmaissistemas.com.br>
2020-05-24 11:34:13 -0300

    Projeto 1 - Estrutura HTML e CSS

--- src/components/Projeto1.vue
+++ src/components/Projeto1.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="Projeto1">
-      
-  </div>
+    <div id="Projeto1">
+        <div class="panel panel-default scores"></div>
+        <div class="panel panel-default result"></div>
+        <div class="panel panel-default buttons"></div>
+        <div class="panel panel-default logs"></div>
+    </div>
 </template>
 
 <script>
@@ -16,5 +19,20 @@ export default {
 </script>
 
 <style>
+
+html {
+    font-family: 'Montserrat', sans-serif;
+}
+
+#Projeto1 {
+    display: flex;
+    flex-direction: column;
+}
+
+.panel {
+    margin: 10px;
+    padding: 20px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.15);
+}
 
 </style>
